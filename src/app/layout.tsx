da3cba988221f2a6016068233db7071dfdc99b33
PredFinance <predfi.xyz@gmail.com>
2025-07-08 12:00:21 -0600

    created an update to the favicon and manifest file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,14 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@700;900&family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
+        {/* Favicon and App Icons */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
       </head>
       <body className="font-body antialiased">
         <Header />
